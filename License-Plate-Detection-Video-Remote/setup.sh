0bd0709de489bfd000a1df2520d5a8d5e01bd3c1
Ray <ray.carroll87@gmail.com>
2023-12-19 20:59:27 +0000

    changes

---
+++ License-Plate-Detection-Video-Remote/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#sudo apt-get update
+#sudo apt-get dist-upgrade
+#sudo apt full-upgrade
+#reboot
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh | bash
+
+source ~/.nvm/nvm.sh
+
+nvm install 4.2.1
+nvm install node
+nvm install 8.10.0
+
+nvm use 8.10.0
+
+sudo chmod -R 777 directory_name
+
+npm install node-fetch@2
+
+npm install log-timestamp
+
+npm install @google-cloud/datastore
+
+sudo apt-get install ffmpeg
